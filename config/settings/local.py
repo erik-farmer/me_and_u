6762c111ccd39c7b245beb5882a8a34d18bfee96
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2018-05-03 15:03:27 -0700

    Add local database settings for docker

--- config/settings/local.py
+++ config/settings/local.py
@@ -10,9 +10,13 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev_db',
+        'USER': 'dev_user',
+        'PASSWORD': 'dev_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
+    }
+}
