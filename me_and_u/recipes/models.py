94e0c94bc1fb03b3314af6d1fe335ed251d1e000
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2019-02-19 14:46:15 -0800

    Improve recipe model

--- me_and_u/recipes/models.py
+++ me_and_u/recipes/models.py
@@ -5,6 +5,10 @@ from django.db import models
 from me_and_u.core.models import TimeStampedModel
 
 class Recipe(TimeStampedModel):
+    name = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
     ingredients = models.TextField()
     steps = models.TextField()
+
+    def __str__(self):
+        return self.name
