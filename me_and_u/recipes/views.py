f97fbbfcc60f6fd2c238e8ecc4b7c28dafbde2db
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2019-03-01 12:29:28 -0800

    improve query for recipe detail view. Also includes explicit context object and template names

--- me_and_u/recipes/views.py
+++ me_and_u/recipes/views.py
@@ -7,6 +7,17 @@ class RecipeList(ListView):
         return Recipe.objects.all().values('public_id', 'name')
 
 class RecipeDetail(DetailView):
+    template_name = 'recipes/recipe_detail.html'
+
+    def get_context_object_name(self, obj):
+        return 'recipe'
+
     def get_object(self):
         recipe_id = self.kwargs['recipe_id']
-        return Recipe.objects.get(public_id=recipe_id)
+        return Recipe.objects.values(
+            'name',
+            'url',
+            'ingredients',
+            'steps',
+            'notes',
+        ).get(public_id=recipe_id)
