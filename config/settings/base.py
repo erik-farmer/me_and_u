7132abcb456d72a85a6bce7d780fe941dcee354a
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2019-03-01 12:01:01 -0800

    remove duplicate DTB from middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,6 @@ PROJECT_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
