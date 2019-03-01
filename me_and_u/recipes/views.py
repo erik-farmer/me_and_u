7cfa1ef763ba304ec0f361d7177ed6e91cfe89c1
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2019-03-01 12:01:21 -0800

    add values to queryset from recipe list view

--- me_and_u/recipes/views.py
+++ me_and_u/recipes/views.py
@@ -5,6 +5,9 @@ from .models import Recipe
 class RecipeList(ListView):
     model = Recipe
 
+    def get_queryset(self):
+        return Recipe.objects.all().values('public_id', 'name')
+
 class RecipeDetail(DetailView):
     def get_object(self):
         recipe_id = self.kwargs['recipe_id']
