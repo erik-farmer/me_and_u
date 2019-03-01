5f64318a2830396058b02de793aa338149600c2a
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2019-03-01 12:02:18 -0800

    remove model attr from list view (get_queryset override handles this)

--- me_and_u/recipes/views.py
+++ me_and_u/recipes/views.py
@@ -3,8 +3,6 @@ from django.views.generic.detail import DetailView
 from .models import Recipe
 
 class RecipeList(ListView):
-    model = Recipe
-
     def get_queryset(self):
         return Recipe.objects.all().values('public_id', 'name')
 
