add0285d37bdd9dd5c3ba112294b8b0631a879e4
Erik Farmer <5082725+erik-farmer@users.noreply.github.com>
2019-03-08 14:29:32 -0800

    fix detail view template block and add heroku to allowed hosts

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,11 +3,14 @@ from .base import *
 import os
 import dj_database_url
 
+ALLOWED_HOSTS += ".herokuapp.com"
+
 DEBUG = False
+
 SECRET_KEY = os.environ['SECRET_KEY']
+
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)
 }
-
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
